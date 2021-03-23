f34117459811a6035194878fde6a3d94eb4c25bb
Fredrik Edman <petiqx@hotmail.com>
2021-03-23 14:26:56 +0100

    Updated the manifest and added icons

--- index.php
+++ index.php
@@ -28,6 +28,11 @@ include('session.php');
   <!-- Startup configuration -->
   <link rel="manifest" crossorigin="use-credentials" href="./manifest.webmanifest">
 
+  <!-- Icons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon-16x16.png">
+
 </head>
 
 <body>
