bb27855df4e56fe9ec09c23f5c652d5034a874c7
Fredrik Edman <petiqx@hotmail.com>
2021-03-23 14:35:26 +0100

    Added favicon

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ include('session.php');
   <link rel="apple-touch-icon" sizes="180x180" href="./img/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon-16x16.png">
+  <link rel="shortcut icon" type="image/x-icon" sizes="16x16" href="./img/favicon.ico">
 
 </head>
 
